8b3e3d6ad50970e3d575254833df6efd92eb440c
Coder <31937370+Code1441@users.noreply.github.com>
2018-02-23 09:13:00 +0300

    code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    var i = 1, count = 0;
+
+    while(Math.pow(5,i)<number){
+        count+=Math.floor(number /Math.pow(5,i));
+        i++;
+    }
+    return count;
 }
+
